53b51c05be8f4f9965fcff54d1775c53cd0a0075
knaut <info@knaut.net>
2015-08-18 18:28:52 -0700

    templateSettings, deleted dead comments

--- src/Store.js
+++ src/Store.js
@@ -24,36 +24,9 @@ var Store = function(obj) {
 	this.initialize.apply(this, arguments);
 };
 
-/*
-	initialize,				// all preliminary setup
-	deinitialize,			// deconstruction, unbinding events, etc
-
-	bindInternals, 			// convenience function for binding internal events (and events the component listens to)
-	registerWithDispatcher, 	// all dispatcher registrations (blank by default)
-
-	compareIdsByLevel, 		// takes two concatenated cids and checks if they match to a certain level, informing us if the two are related.
-								// this is particularly useful in event handlers when listening to blanket dispatcher events.
-								// use this function when you want to block a store's process because the dispatcher's message was irrelevant based
-								// on the component parent-child hierarchy
-
-	getCurrentState,				// needs work, should provide a copied object of this store's state
-	getCurrentProps,				// needs work, should provide a copied object of this store's props
-
-	setState,				// takes an incoming object, sets state properties based on that object. if successful, fires off an event
-	setProps,				// takes an incoming object, sets state properties based on that object. if successful, fires off an event
-
-	shouldComponentUpdate,	// takes the current state (props?) and a mutated version of the same object, compares the two, and if they are different, fires an event
-	startLifecycle,			// kicks off the store's update process, firing the specified functions in a queue
-
-	startUpdate,			// update process part 1
-	onUpdate,				// update process part 2 (most often used)
-	afterUpdate			// any post-update processes
-	
-*/
-
 _.extend(Store.prototype, Events, {
 	initialize: function() {
-		this.id = _.uniqueId('s');
+		this.id = this.parent.id + _.uniqueId('s');
 		this.setProps(this.model);
 
 		// initial state from constructor args
