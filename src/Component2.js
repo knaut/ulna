4f12da2de9baf33bd88e456c223378026c4f1f86
_knaut <info@knaut.net>
2015-12-29 01:26:54 -0800

    Component2 thinking about new api with nerve templates

---
+++ src/Component2.js
@@ -0,0 +1,56 @@
+var component = function( obj ) {
+	// root selector and jquery ref
+	this.root = null;
+	this.$root = null;
+
+	// nerve template structure
+	this.template = null;
+
+	// one update cycle to rule them all
+	this.updateCycle = [
+		'beforeUpdate',
+		'onUpdate',
+		'afterUpdate'
+	];
+	this.updateCycleTimeout = true;
+
+	// id addresses
+	this.id = _.uniqueId('c');
+
+	// children 
+	this.children = [];
+
+	// ui event hashes
+	this.events = {};
+
+	// listen hash for dispatcher hooks
+	this.listen = {};
+
+	// apply arguments
+	for (var prop in obj) {
+		this[prop] = obj[prop]
+	}
+
+	this.initialize.apply(this, arguments);
+}
+
+component.prototype = {
+	initialize: function() {
+
+	},
+	deinitialize: function() {
+
+	},
+	bindEvents: function() {
+
+	},
+	unbindEvents: function() {
+
+	},
+	bindListen: function() {
+
+	},
+	unbindListen: function() {
+
+	}
+}
